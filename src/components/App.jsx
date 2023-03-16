64a9d8d25327251202260417124e4bf996f007db
Ruslan379 <arnov@i.ua>
2023-03-16 16:19:33 +0200

    Trigger FOLLOW - 6

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,47 @@
+import { useState } from 'react';
+import css from './App.module.css';
+
 export const App = () => {
+  let followers = 100500
+
+  const [trigger, setTrigger] = useState(false);
+  console.log("trigger:", trigger);
+
+  if (trigger) followers = 100501
+
+  const toggleTrigger = () => {
+    setTrigger(!trigger);
+    // console.log("toggleTrigger --> ", trigger);
+  };
+
+  
+  
   return (
+
     <div
       style={{
         height: '100vh',
         display: 'flex',
+        flexDirection: "column",
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101'
       }}
     >
-      React homework template
+      <p>777 tweets</p>
+      <p>FOLLOWERS <span className={css.spanFollowers}>{followers}</span></p>
+      <button
+          type="button"
+          className={css.btnFollowers}
+          onClick={toggleTrigger}
+      >
+        FOLLOW
+        {/* {isLoading ? [<Spinner size="18" />, " Editing..."] : "Edit"} */}
+      </button>
+
+
+     
     </div>
   );
 };
