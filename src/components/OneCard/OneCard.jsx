bab1884a5658cf295da935b319fac5af515ac5fb
Ruslan379 <arnov@i.ua>
2023-03-21 20:40:58 +0200

    Styling with styled-components <OneCard/> - 50

--- src/components/OneCard/OneCard.jsx
+++ src/components/OneCard/OneCard.jsx
@@ -1,23 +1,25 @@
 import useLocalStorage from '../hooks/useLocalStorage';
-
 import { ButtonTrigger } from '../ButtonTrigger/ButtonTrigger.jsx';
-
 import logo from 'images/logo.svg';
 import { ReactComponent as Ellipse } from 'images/ellipse.svg';
 import avatar from 'images/avatar.svg'; 
 import rectangle from 'images/rectangle.svg'; 
-
-import css from './OneCard.module.css';
-
+import {
+  Card,
+  Logo,
+  RoundFrameAvatar, 
+  Avatar,
+  Rectangle,
+  Tweets,
+  Followers
+} from './OneCard.styled';
 
 export const OneCard = () => {
   const [triggerOneCard, setTriggerOneCard] = useLocalStorage("TriggerOneCard", false);
 
-  //! Input data
   const tweets = 777;
   let followers = 100500;
 
-  //! --------------- Преобразование 100500(100501) --> 100,500(100,501) ---------------
   let followersString = followers.toString();
   if (triggerOneCard) {
     followers = followers + 1;
@@ -26,45 +28,35 @@ export const OneCard = () => {
   let followersRenderStart = followersString.slice(0, -3);
   let followersRenderEnd = followersString.slice(-3);
   
-
   const toggleTriggerOneCard = () => {
     setTriggerOneCard(!triggerOneCard);
   };
 
   return (
-    <div className={css.card}>
-      {/* //! logo */}
-      <img className={css.logo}
+    <Card>
+      <Logo
         alt="first logo"
         src={logo}
         width="76"
       />
-      {/* //! ellipse */}
-      <Ellipse className={css.ellipse} />
-      {/* //! avatar */}
-      <img className={css.avatar}
-        alt="avatar"
-        src={avatar}
-        width="63"
-      />
-      {/* //! rectangle */}
-      <img className={css.rectangle}
+      <Rectangle
         alt="rectangle"
         src={rectangle}
         width="380"
       />
-      {/* //! tweets */}
-      <p className={css.tweets}
-      >
-        {tweets} tweets
-      </p>
-      {/* //! FOLLOWERS */}
-      <p className={css.followers}
-      >
-        <span className={css.spanFollowers}>{followersRenderStart},{followersRenderEnd} </span>
+      <RoundFrameAvatar>
+        <Ellipse />
+      </RoundFrameAvatar>
+      <Avatar
+        alt="avatar"
+        src={avatar}
+        width="63"
+      />
+      <Tweets>{tweets} tweets</Tweets>
+      <Followers>
+        <span>{followersRenderStart},{followersRenderEnd} </span>
         FOLLOWERS
-      </p>
-      {/* //! Trigger button */}
+      </Followers>
       <ButtonTrigger
         trigger={triggerOneCard}
         toggleTrigger={toggleTriggerOneCard}
@@ -72,6 +64,6 @@ export const OneCard = () => {
         textFalse={"FOLLOW"}
         inversionBackColor={false}
       />
-    </div>
+    </Card>
   );
 };
---
+++ src/components/OneCard/OneCard.styled.js
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+import picture from '../../images/picture.svg';
+
+
+export const Card = styled.div`
+    position: relative;
+    height: 460px;
+    width: 380px;
+    margin-top: 40px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 24px;
+    text-transform: uppercase;
+    color: #EBD8FF;
+    box-shadow: -2.5777px 6.87386px 20.6216px rgba(0, 0, 0, 0.23);
+    border-radius: 20px;
+    background-image: url(${picture}),
+        linear-gradient(
+            114.99deg, 
+            #471CA9 -0.99%, 
+            #5736A3 54.28%, 
+            #4B2A99 78.99%
+            );
+        background-repeat: no-repeat;
+        background-size: 308px 168px, cover;
+        background-position-x: center;
+        background-position-y: 28px, center;
+`;
+
+export const Logo = styled.img`
+    position: absolute;
+        top: 20px;
+        left: 20px;
+`;
+
+export const RoundFrameAvatar = styled.div`
+    position: absolute;
+    top: 178px;
+    z-index: 99;
+`;
+
+export const Avatar = styled.img`
+    position: absolute;
+    top: 187px;
+    z-index: 77;
+    border-radius: 50%;
+    background: linear-gradient(
+            114.99deg, 
+            #471CA9 -0.99%, 
+            #5736A3 54.28%, 
+            #4B2A99 78.99%
+            );
+`;
+
+export const Rectangle = styled.img`
+    position: absolute;
+        top: 214px;
+`;
+
+export const Tweets = styled.p`
+    margin-top: 284px;
+`;
+
+export const Followers = styled.p`
+    margin-top: 16px;
+`;
+
+
